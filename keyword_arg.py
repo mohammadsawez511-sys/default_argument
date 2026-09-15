c92633649bac41788c107c24382f22a92b6517e8
mohammadsawez511-sys <mohammadsawez511@gmail.com>
2026-09-15 22:32:10 +0530

    Add files via upload

---
+++ keyword_arg.py
@@ -0,0 +1,7 @@
+def student(name,roll_number,marks,college_name ):
+    print("name =",name)
+    print("college name =",college_name)
+    print("roll number =",roll_number)
+    print("marks =",marks)
+    
+student(name="sawez",college_name="ATT",marks =[15,15],roll_number=22)
